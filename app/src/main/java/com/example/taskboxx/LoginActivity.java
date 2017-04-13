16ac2e17ca6440069b9cf8b717794cb1b878a427
Shubham Maheshwari <shub97@gmail.com>
2017-04-13 23:28:47 +0530

    Done With the Login and the Sign Up

--- app/src/main/java/com/example/taskboxx/LoginActivity.java
+++ app/src/main/java/com/example/taskboxx/LoginActivity.java
@@ -1,17 +1,23 @@
 package com.example.taskboxx;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        textView = (TextView) findViewById(R.id.launch_forgotpass);
+        textView.setPaintFlags(textView.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     }
 
     public void Login(View view){
@@ -24,5 +30,10 @@ public class LoginActivity extends AppCompatActivity {
         Toast.makeText(this, "Great Idea!", Toast.LENGTH_SHORT).show();
         Intent signupintent = new Intent(this,SignUpActivity.class);
         startActivity(signupintent);
+    }
+
+    public void forgotPass(View view){
+        Toast.makeText(this, "No problem!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "But the feature isn't implemented yet", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/taskboxx/SignUpActivity.java
+++ app/src/main/java/com/example/taskboxx/SignUpActivity.java
@@ -1,46 +1,26 @@
 package com.example.taskboxx;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
-
-    Toolbar toolbar;
-    TabLayout tabLayout;
-    ViewPager viewPager;
-    viewpageradapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        toolbar = (Toolbar) findViewById(R.id.toolBar);
-        setSupportActionBar(toolbar);
-        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
-        viewPager = (ViewPager) findViewById(R.id.viewPager);
-        viewPagerAdapter = new viewpageradapter(getSupportFragmentManager());
-        viewPagerAdapter.addFragments(new StudentSignUpFragment(),"Student");
-        viewPagerAdapter.addFragments(new FacultySignUpFragment(),"Faculty");
-        viewPager.setAdapter(viewPagerAdapter);
-        tabLayout.setupWithViewPager(viewPager);
-
     }
 
-    public void StudentSignUp(View view){
-        Toast.makeText(this, "Registered as a Student!", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "Please Log In to Proceed", Toast.LENGTH_SHORT).show();
-        Intent loginintent = new Intent(this,LoginActivity.class);
-        startActivity(loginintent);
-    }
-
-    public void FacultySignUp(View view){
-        Toast.makeText(this, "Registered as a Faculty!", Toast.LENGTH_SHORT).show();
+    public void SignUp(View view){
+        Toast.makeText(this, "Registered!", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "Please Log In to Proceed", Toast.LENGTH_SHORT).show();
         Intent loginintent = new Intent(this,LoginActivity.class);
         startActivity(loginintent);
